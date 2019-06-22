a219b2b4e9c8782b04faab3f53ee74e94caef1db
yi <193463771@qq.com>
2019-06-22 14:13:06 +0800

    1.1

--- src/main/java/com/yi/Interact/SignInUi.java
+++ src/main/java/com/yi/Interact/SignInUi.java
@@ -9,7 +9,7 @@ public class SignInUi {
     }
     private void initJframe(){
         this.jFrame=new JFrame("欢迎界面");
-        this.jFrame.setSize(300,200);
+        this.jFrame.setSize(600,400);
         this.jFrame.setVisible(true);
         this.jFrame.setResizable(false);
         this.jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
